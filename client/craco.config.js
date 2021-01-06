36f86c44226342bf26252f09e0d081dab053c1ba
Giordano Battilana <giordano.battilana@novadart.com>
2021-01-06 17:45:05 +0100

    added Ant Design

--- client/craco.config.js
+++ client/craco.config.js
@@ -15,41 +15,20 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-.App {
-  text-align: center;
-}
+const CracoLessPlugin = require('craco-less');
 
-.App-logo {
-  height: 40vmin;
-  pointer-events: none;
-}
-
-@media (prefers-reduced-motion: no-preference) {
-  .App-logo {
-    animation: App-logo-spin infinite 20s linear;
-  }
-}
-
-.App-header {
-  background-color: #282c34;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
-}
-
-.App-link {
-  color: #61dafb;
-}
-
-@keyframes App-logo-spin {
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-}
+module.exports = {
+    plugins: [
+        {
+            plugin: CracoLessPlugin,
+            options: {
+                lessLoaderOptions: {
+                    lessOptions: {
+                        modifyVars: {'@primary-color': '#F58D32', '@link-color': '#58D41C'},
+                        javascriptEnabled: true,
+                    },
+                },
+            },
+        },
+    ],
+};
